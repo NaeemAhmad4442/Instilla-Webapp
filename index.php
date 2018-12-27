acb185725c838a88c89ea6c10c10e23a32d770bb
Naeem <naeem.44@hotmail.it>
2018-12-27 19:00:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -95,7 +95,7 @@ if(isset($_POST["sub"]))
   				</div>
 			</center>
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-4 col-md-offset-4">
 			<center>
 			Total characters: <input type="number" readonly="" name="" style="width:50; height: 30" id="total" /><br>
 			Sum of indexes: <input type="number" readonly="" name="" style="width:50; height: 30" id="indexes" />
